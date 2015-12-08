9eccb927f9afeb569f6e5367d299b50bc99aeab9
Victor Lopez <victorcete@gmail.com>
2015-12-08 18:42:07 +0100

    Added OSX params setup script

---
+++ osx.sh
@@ -0,0 +1,217 @@
+#!/usr/bin/env bash
+
+############################################################
+# Bootstrap script for Mac OS X El Capitan new installations
+# @author Victor Lopez
+############################################################
+
+# Include bash helpers
+# https://github.com/atomantic/dotfiles/blob/master/lib.sh
+source misc/lib.sh
+
+# Say hello
+bot "New OS X installation! Please enter your sudo password before proceeding"
+
+# Ask for the sudo password
+sudo -v
+
+# update existing `sudo` time stamp until the script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+running "Disable local Time Machine snapshots"
+sudo tmutil disablelocal;ok
+
+running "always boot in verbose mode (not OSX GUI mode)"
+sudo nvram boot-args="-v";ok
+
+running "Disable the sound effects on boot"
+sudo nvram SystemAudioVolume=" ";ok
+
+running "Always show scrollbars"
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always";ok
+
+running "Automatically quit printer app once the print jobs complete"
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true;ok
+
+running "Disable the crash reporter"
+defaults write com.apple.CrashReporter DialogType -string "none";ok
+
+running "Never go into computer sleep mode"
+sudo systemsetup -setcomputersleep Off > /dev/null;ok
+
+running "Check for software updates daily, not just once per week"
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1;ok
+
+running "Disable Notification Center and remove the menu bar icon"
+launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist > /dev/null 2>&1;ok
+
+running "Disable smart quotes as they’re annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false;ok
+
+running "Disable smart dashes as they’re annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false;ok
+
+running "Disable “natural” (Lion-style) scrolling"
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false;ok
+
+running "Disable press-and-hold for keys in favor of key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false;ok
+
+running "Set a blazingly fast keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 0;
+
+running "Set a shorter delay until key repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 15;
+
+running "Disable auto-correct"
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false;ok
+
+running "Require password immediately after sleep or screen saver begins"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0;ok
+
+running "Save screenshots to the desktop"
+defaults write com.apple.screencapture location -string "${HOME}/Desktop";ok
+
+running "Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)"
+defaults write com.apple.screencapture type -string "png";ok
+
+running "Disable window animations and Get Info animations"
+defaults write com.apple.finder DisableAllAnimations -bool true;ok
+
+running "Set Desktop as the default location for new Finder windows"
+# For other paths, use `PfLo` and `file:///full/path/here/`
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/";ok
+
+running "Show hidden files by default"
+defaults write com.apple.finder AppleShowAllFiles -bool true;ok
+
+running "Show all filename extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true;ok
+
+running "Show status bar"
+defaults write com.apple.finder ShowStatusBar -bool true;ok
+
+running "Show path bar"
+defaults write com.apple.finder ShowPathbar -bool true;ok
+
+running "Allow text selection in Quick Look"
+defaults write com.apple.finder QLEnableTextSelection -bool true;ok
+
+running "Display full POSIX path as Finder window title"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true;ok
+
+running "When performing a search, search the current folder by default"
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf";ok
+
+running "Disable the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false;ok
+
+running "Enable spring loading for directories"
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true;ok
+
+running "Remove the spring loading delay for directories"
+defaults write NSGlobalDomain com.apple.springing.delay -float 0;ok
+
+running "Avoid creating .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true;ok
+
+running "Disable disk image verification"
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true;ok
+
+running "Automatically open a new Finder window when a volume is mounted"
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true;ok
+
+running "Use list view in all Finder windows by default"
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv";ok
+
+running "Disable the warning before emptying the Trash"
+defaults write com.apple.finder WarnOnEmptyTrash -bool false;ok
+
+running "Empty Trash securely by default"
+defaults write com.apple.finder EmptyTrashSecurely -bool true;ok
+
+running "Show the ~/Library folder"
+chflags nohidden ~/Library;ok
+
+running "Expand the following File Info panes: “General”, “Open with”, and “Sharing & Permissions”"
+defaults write com.apple.finder FXInfoPanesExpanded -dict \
+	General -bool true \
+	OpenWith -bool true \
+	Privileges -bool true;ok
+
+running "Set the icon size of Dock items to 36 pixels"
+defaults write com.apple.dock tilesize -int 36;ok
+
+running "Change minimize/maximize window effect to scale"
+defaults write com.apple.dock mineffect -string "scale";ok
+
+running "Minimize windows into their application’s icon"
+defaults write com.apple.dock minimize-to-application -bool true;ok
+
+running "Enable spring loading for all Dock items"
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true;ok
+
+running "Show indicator lights for open applications in the Dock"
+defaults write com.apple.dock show-process-indicators -bool true;ok
+
+running "Don’t animate opening applications from the Dock"
+defaults write com.apple.dock launchanim -bool false;ok
+
+running "Speed up Mission Control animations"
+defaults write com.apple.dock expose-animation-duration -float 0.1;ok
+
+running "Don’t group windows by application in Mission Control"
+# (i.e. use the old Exposé behavior instead)
+defaults write com.apple.dock expose-group-by-app -bool false;ok
+
+running "Disable Dashboard"
+defaults write com.apple.dashboard mcx-disabled -bool true;ok
+
+running "Don’t show Dashboard as a Space"
+defaults write com.apple.dock dashboard-in-overlay -bool true;ok
+
+running "Don’t automatically rearrange Spaces based on most recent use"
+defaults write com.apple.dock mru-spaces -bool false;ok
+
+running "Remove the auto-hiding Dock delay"
+defaults write com.apple.dock autohide-delay -float 0;ok
+running "Remove the animation when hiding/showing the Dock"
+defaults write com.apple.dock autohide-time-modifier -float 0;ok
+
+running "Automatically hide and show the Dock"
+defaults write com.apple.dock autohide -bool true;ok
+
+running "Make Dock icons of hidden applications translucent"
+defaults write com.apple.dock showhidden -bool true;ok
+
+running "Make Dock more transparent"
+defaults write com.apple.dock hide-mirror -bool true;ok
+
+running "Top left screen corner → Mission Control"
+defaults write com.apple.dock wvous-tl-corner -int 2
+defaults write com.apple.dock wvous-tl-modifier -int 0;ok
+running "Top right screen corner → Mission Control"
+defaults write com.apple.dock wvous-tr-corner -int 2
+defaults write com.apple.dock wvous-tr-modifier -int 0;ok
+running "Bottom right screen corner → Desktop"
+defaults write com.apple.dock wvous-br-corner -int 4
+defaults write com.apple.dock wvous-br-modifier -int 0;ok
+running "Bottom left screen corner → Desktop"
+defaults write com.apple.dock wvous-bl-corner -int 4
+defaults write com.apple.dock wvous-bl-modifier -int 0;ok
+
+bot "OK. Note that some of these changes require a logout/restart to take effect. Killing affected applications (so they can reboot)...."
+for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+	"Dock" "Finder" "Mail" "Messages" "Safari" "SizeUp" "SystemUIServer" \
+	"iCal" "Terminal"; do
+	killall "${app}" > /dev/null 2>&1
+done
+
+exit 0
